0d6c991780dd4f3706ea3f4ebd2140fb351cc84f
Andrii Rybak <adnrii.rybak@amomedia.com>
2023-06-14 17:38:03 +0300

    Deleted files from main

--- src/main/java/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-
-    public static void main(String[] args) {
-
-        AppImpl appImpl = new AppImpl();
-        int returnedValue = appImpl.printSomeInt();
-        System.out.println("~~~" + returnedValue + "~~~");
-    }
-}
--- src/main/java/AppImpl.java
+++
@@ -1,9 +0,0 @@
-public class AppImpl {
-
-    private int someInt = 10;
-
-    public int printSomeInt() {
-        System.out.println("---" + someInt + "---");
-        return someInt;
-    }
-}
